8ba5c10a944cdc9b89ee17d46af86aa1de6cb056
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-16 07:49:39 +0530

    first commit

---
+++ src/UserRegistrationLamda.java
@@ -0,0 +1,23 @@
+import java.util.regex.Pattern;
+
+@FunctionalInterface
+interface UserRegistrationInterface {
+    boolean validate(String details,String pattern);
+}
+public class UserRegistrationLamda {
+    public static void main(String[] args) {
+        UserRegistrationInterface firstName = (String pattern, String first_name) -> {
+            if (Pattern.matches(pattern, first_name)) {
+                System.out.println("Entered Name is Valid");
+                return true;
+
+            } else {
+                System.out.println("Entered Name is Invalid");
+                return false;
+            }
+        };
+        firstName.validate("^[A-Z][a-z]{2,}$", "Jagan");
+        firstName.validate("^[A-Z][a-z]{2,}$", "saran");
+
+    }
+}
