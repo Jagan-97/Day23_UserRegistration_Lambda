52194960fba8c8ebeddbc0f7abeb26c5144e9e0b
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-16 08:08:20 +0530

    Added Firstname_Validate Logic

--- src/UserRegistrationLamda.java
+++ src/UserRegistrationLamda.java
@@ -2,22 +2,22 @@ import java.util.regex.Pattern;
 
 @FunctionalInterface
 interface UserRegistrationInterface {
-    boolean validate(String details,String pattern);
+    boolean validate(String pattern, String details);
 }
+
 public class UserRegistrationLamda {
+
     public static void main(String[] args) {
         UserRegistrationInterface firstName = (String pattern, String first_name) -> {
             if (Pattern.matches(pattern, first_name)) {
                 System.out.println("Entered Name is Valid");
                 return true;
-
             } else {
                 System.out.println("Entered Name is Invalid");
                 return false;
             }
         };
         firstName.validate("^[A-Z][a-z]{2,}$", "Jagan");
-        firstName.validate("^[A-Z][a-z]{2,}$", "saran");
-
+        firstName.validate("^[A-Z][a-z]{2,}$", "jagan");
     }
 }
