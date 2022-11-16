e9806c9a182f56336ad19e53d63f77d747557ae6
Jagan-97 <jagan.psaran38@gmail.com>
2022-11-16 08:18:04 +0530

    Added LastName_Validate logic

--- src/UserRegistrationLamda.java
+++ src/UserRegistrationLamda.java
@@ -12,12 +12,27 @@ public class UserRegistrationLamda {
             if (Pattern.matches(pattern, first_name)) {
                 System.out.println("Entered Name is Valid");
                 return true;
+
             } else {
                 System.out.println("Entered Name is Invalid");
                 return false;
             }
         };
+        UserRegistrationInterface lastName = (String pattern, String last_name) -> {
+            if (Pattern.matches(pattern, last_name)) {
+                System.out.println("Entered Name is Valid");
+                return true;
+            }else {
+                System.out.println("Entered Name is Invalid");
+                return false;
+            }
+        };
+
         firstName.validate("^[A-Z][a-z]{2,}$", "Jagan");
         firstName.validate("^[A-Z][a-z]{2,}$", "jagan");
+
+        lastName.validate("^[A-Z][a-z]{2,}$", "Saran");
+        lastName.validate("^[A-Z][a-z]{2,}$", "sara");
+
     }
 }
